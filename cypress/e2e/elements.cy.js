fcda3ae20bb8408727d6f38d5410cbe99e263928
Rodrigo Cespedes <rscespedesm@gmail.com>
2023-08-05 11:21:31 -0400

    Adding:     *Fixtures data     *validations for first test     *First Test Case Finished Changes to be committed: 	modified:   cypress/e2e/elements.cy.js 	new file:   cypress/fixtures/elementsFixture.json 	modified:   cypress/pages/ElementsPage.js

--- cypress/e2e/elements.cy.js
+++ cypress/e2e/elements.cy.js
@@ -8,21 +8,25 @@ describe('Suit to test elements in demo qa web page', () => {
         return false
       })
 
+
     before(() => {
       cy.visit("https://demoqa.com/")
     })
+
+    beforeEach(() => {
+      cy.fixture('elementsFixture')
+      .then(function(elementsData) { 
+        this.elementsData = elementsData;
+      })
+    })
   
-    it.only('Positive Textbox test case', () => {
+    it.only('Positive Textbox test case', function() {
       MainPage.clickOnElementsCard();
       ElementsPage.elements.textboxButton().click()
-      var data = {
-        name: "Rodrigo Cespedes",
-        email: "rodrigo.cespedes@email.com",
-        currentAddress: "Viktoria Street # 548",
-        permanentAddress: "Viktoria Street # 548"
-        }
-
-    ElementsPage.populateForm(data);
-    
+      ElementsPage.populateForm(this.elementsData);
+      ElementsPage.elements.nameText().should('contains.text', this.elementsData.name)
+      ElementsPage.elements.emailText().should('contains.text', this.elementsData.email)  
+      ElementsPage.elements.currentAddressText().should('contains.text', this.elementsData.currentAddress)  
+      ElementsPage.elements.permanentAddressText().should('contains.text', this.elementsData.permanentAddress)  
     })
   })
--- cypress/pages/ElementsPage.js
+++ cypress/pages/ElementsPage.js
@@ -1,16 +1,26 @@
 class ElementsPage{
     elements = {
     textboxButton : () => cy.get(".element-list > .menu-list > li .text").contains("Text Box"),
-    userForm : () => cy.get('#userForm')
+    userNameTextbox : () => cy.get('#userName'),
+    userEmailTextbox : () => cy.get('#userEmail'),
+    currentAddressTextbox : () => cy.get('#currentAddress'),
+    permanentAddressTextbox : () => cy.get('#permanentAddress'),
+    submitButton : () => cy.get('#submit'),
+    userForm : () => cy.get('#userForm'),
+    registryBox : () => cy.get('.border'),
+    nameText : () => cy.get('#name'),
+    emailText : () => cy.get('#email'),
+    currentAddressText : () => cy.get('.border > #currentAddress'),
+    permanentAddressText : () => cy.get('.border > #permanentAddress')
     }
 
     populateForm(data){
         this.elements.userForm().within(() => {
-            cy.get('#userName').type(data.name)
-            cy.get('#userEmail').type(data.email)
-            cy.get('#currentAddress').type(data.name)
-            cy.get('#permanentAddress').type(data.name)
-            cy.get("#submit").click()
+            this.elements.userNameTextbox().type(data.name)
+            this.elements.userEmailTextbox().type(data.email)
+            this.elements.currentAddressTextbox().type(data.currentAddress)
+            this.elements.permanentAddressTextbox().type(data.permanentAddress)
+            this.elements.submitButton().click()
         })
     }
 
